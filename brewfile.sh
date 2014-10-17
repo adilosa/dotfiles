2197c2685f395f47cca8e35ab1b496cc714a6bbb
Andrew DiLosa <Andrew@Andrews-MacBook-Pro.local>
2014-10-16 20:53:02 -0700

    Updated homebrew files to shell scripts, as bundle is no longer supported

--- brewfile.sh
+++ brewfile.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Install command-line tools using Homebrew
 
 # Make sure we’re using the latest Homebrew
---
+++ caskfile.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install native apps
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install google-chrome 2> /dev/null
+brew cask install imagealpha 2> /dev/null
+brew cask install imageoptim 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install macvim 2> /dev/null
+brew cask install the-unarchiver 2> /dev/null
+brew cask install transmission 2> /dev/null
+brew cask install vlc 2> /dev/null
